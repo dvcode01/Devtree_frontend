5012d487ba7ab92e9a561a8b56fa452254447e89
David <daviolivero2103@gmail.com>
2025-12-31 16:01:52 -0400

    feat(views): add routing for views

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./views/auth/Login"
+import Register from "./views/auth/Register"
 
 function Router() {
   return (
-    <>
-        
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
