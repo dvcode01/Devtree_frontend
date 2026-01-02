8c8108d0ffaa3e6b104cb6ddc937ef332e216553
David <daviolivero2103@gmail.com>
2026-01-01 21:44:10 -0400

    style(layouts): add styles to navigation

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 function Login() {
   return (
     <>
-      <h1 className="text-white">Login</h1>
+      <h1 className="text-white text-4xl font-bold">Iniciar Sesión</h1>
 
-      <nav>
+      <nav className="mt-10">
 
-        <Link to="/auth/register">
+        <Link to="/auth/register" className="text-center text-white text-lg block">
           ¿No tienes cuenta? Crea una
         </Link>
       </nav>
--- src/views/auth/Register.tsx
+++ src/views/auth/Register.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 function Register() {
   return (
     <>
-      <h1 className="text-white">Register</h1>
+      <h1 className="text-white text-4xl font-bold">Crear Cuenta</h1>
 
-      <nav>
-        <Link to="/auth/login">
+      <nav className="mt-10">
+        <Link to="/auth/login" className="text-center text-white text-lg block">
           ¿Ya tienes una cuenta? Inicia Sesión
         </Link>
       </nav>
