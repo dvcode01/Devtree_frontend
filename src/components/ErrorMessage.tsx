24548b8e0621922bb92e3370bf5ab2c05ccc7c52
David <daviolivero2103@gmail.com>
2026-01-02 15:08:44 -0400

    feat: create a component to display errors

---
+++ src/components/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+
+type ErrorMessageProps = {
+    children: React.ReactNode
+}
+
+function ErrorMessage({children}: ErrorMessageProps) {
+  return (
+    <p className="bg-red-100 text-red-600 p-3 uppercase text-sm font-bold text-center">{children}</p>
+  )
+}
+
+export default ErrorMessage
--- src/views/auth/Register.tsx
+++ src/views/auth/Register.tsx
@@ -1,12 +1,28 @@
+import { useForm } from "react-hook-form"
 import { Link } from "react-router-dom"
+import ErrorMessage from "../../components/ErrorMessage";
 
 function Register() {
+  const initialValues = {
+    name: '',
+    email: '',
+    handle: '',
+    password: '',
+    password_confirmation: ''
+  };
+
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({defaultValues: initialValues});
+
+  const handleRegister = () => {
+    console.log('enviando..');
+  };
+
   return (
     <>
       <h1 className="text-white text-4xl font-bold">Crear Cuenta</h1>
       
       <form 
-        onSubmit={() => {}}
+        onSubmit={handleSubmit(handleRegister)}
         className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
       >
         <div className="grid grid-cols-1 space-y-3">
@@ -16,7 +32,10 @@ function Register() {
                 type="text"
                 placeholder="Tu Nombre"
                 className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('name', {required: 'El nombre es obligatorio'})}
             />
+
+            {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
         </div>
         <div className="grid grid-cols-1 space-y-3">
             <label htmlFor="email" className="text-2xl text-slate-500">E-mail</label>
@@ -25,7 +44,9 @@ function Register() {
                 type="email"
                 placeholder="Email de Registro"
                 className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('email', {required: 'El email es obligatorio'})}
             />
+            {errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage>}
         </div>
         <div className="grid grid-cols-1 space-y-3">
             <label htmlFor="handle" className="text-2xl text-slate-500">Handle</label>
@@ -34,7 +55,10 @@ function Register() {
                 type="text"
                 placeholder="Nombre de usuario: sin espacios"
                 className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('handle', {required: 'El handle es obligatorio'})}
             />
+
+            {errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage>}
         </div>
         <div className="grid grid-cols-1 space-y-3">
             <label htmlFor="password" className="text-2xl text-slate-500">Password</label>
@@ -43,17 +67,23 @@ function Register() {
                 type="password"
                 placeholder="Password de Registro"
                 className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('password', {required: 'El password es obligatorio'})}
             />
+
+            {errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage>}
         </div>
 
         <div className="grid grid-cols-1 space-y-3">
             <label htmlFor="password_confirmation" className="text-2xl text-slate-500">Repetir Password</label>
             <input
-                id="password"
+                id="password_confirmation"
                 type="password"
                 placeholder="Repetir Password"
                 className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                {...register('password_confirmation', {required: 'Repetir password es obligatorio'})}
             />
+
+            {errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage>}
         </div>
 
         <input
