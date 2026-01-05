cf8f72eeecaad5953ad4b6a72367ad8f8e82e93f
David <daviolivero2103@gmail.com>
2026-01-05 19:44:55 -0400

    feat(api): obtaining user data from the api

---
+++ src/api/DevtreeApi.ts
@@ -0,0 +1,20 @@
+import { isAxiosError } from "axios";
+import api from "../config/api";
+
+export async function getUser(){
+    const token = localStorage.getItem('AUTH_TOKEN');
+
+    try{
+      const { data } = await api('/user', {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+      });
+      
+      return data;
+    } catch (error) {
+      if(isAxiosError(error) && error.response){
+        throw new Error(error.response.data.error);
+      }
+    }
+}
