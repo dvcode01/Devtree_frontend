5c81824c88b2a458511d745a05c5102246d43cfc
David <daviolivero2103@gmail.com>
2026-01-13 18:22:48 -0400

    fix(views): profile information update

--- src/views/pages/ProfileView.tsx
+++ src/views/pages/ProfileView.tsx
@@ -24,6 +24,9 @@ function ProfileView() {
       queryClient.invalidateQueries({
         queryKey: ['user']
       })
+    },
+    onMutate: async () => {
+      queryClient.invalidateQueries({ queryKey: ['user'] });
     }
   });
 
