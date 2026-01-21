96cd52eccc2de6bfd807fb6dbbb7da7d5c30c1e1
David <daviolivero2103@gmail.com>
2026-01-20 21:06:21 -0400

    chore(api): retrieve user data using handle

--- src/views/pages/HandleView.tsx
+++ src/views/pages/HandleView.tsx
@@ -1,6 +1,20 @@
-
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom"
+import { getUserByHandle } from "../../api/DevtreeApi";
 
 function HandleView() {
+  const params = useParams();
+  const handle = params.handle!;
+  
+  const { data, error, isLoading } = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ['handle', handle]
+  });
+
+  console.log(data);
+  console.log(error);
+  console.log(isLoading);
+
   return (
     <div>HandleView</div>
   )
