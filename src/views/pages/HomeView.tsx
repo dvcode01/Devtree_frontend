650b3bab7ed81462312f29a22c00c8cc7b718e68
David <daviolivero2103@gmail.com>
2026-01-22 18:42:56 -0400

    chore(views): add content in Homepage

--- src/views/pages/HomeView.tsx
+++ src/views/pages/HomeView.tsx
@@ -3,8 +3,21 @@ import Header from "../../components/Header"
 function HomeView() {
   return (
     <>
-        <Header />
-    
+      <Header />
+
+      <main className="bg-gray-100 py-10 min-h-screen lg:bg-[url(/bg.svg)] bg-size-[50%] bg-no-repeat bg-top-right">
+        <div className="max-w-5xl mx-auto mt-10">
+          <div className="lg:w-1/2 px-10 lg:p-0 space-y-6">
+            <h1 className="text-6xl font-black">
+              Todas tus <span className="text-cyan-400">Redes Sociales </span>
+              en un enlace
+            </h1>
+
+            <p className="text-slate-800 text-xl">Unete a más de 200 mil developers compartiendo sus redes sociales, comparte tu perfil de Tiktok, Instagram, Youtube, Github y más</p>
+          </div>
+        </div>
+      </main>
+
     </>
   )
 }
